d5795b6bc9da48eacc9daca002d32a6002dca49b
Oguzz <oguzaslan_203@hotmail.com>
2024-03-06 00:47:18 +0300

    Controller and @Qualifier annotation added

---
+++ spring-core/src/main/java/com/example/springcore/controller/DemoController.java
@@ -0,0 +1,22 @@
+package com.example.springcore.controller;
+
+import com.example.springcore.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    private Coach coach;
+    @Autowired
+    public DemoController(@Qualifier("footballCoach") Coach coach) {
+        this.coach = coach;
+    }
+
+    @GetMapping("/getDailyWorkout")
+    public String getDailyWorkout() {
+        return coach.getDailyWorkout();
+    }
+}
