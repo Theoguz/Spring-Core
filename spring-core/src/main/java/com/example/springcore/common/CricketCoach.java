02067d3210f08c7b4e60a1469cbcb9fca0b430e8
Oguzz <oguzaslan_203@hotmail.com>
2024-03-06 02:00:27 +0300

    @PostConstruct @PreDestroy shown

--- spring-core/src/main/java/com/example/springcore/common/CricketCoach.java
+++ spring-core/src/main/java/com/example/springcore/common/CricketCoach.java
@@ -1,9 +1,28 @@
 package com.example.springcore.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CricketCoach implements Coach{
+public class CricketCoach implements Coach {
+
+    public CricketCoach() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    // define our init mmthod
+
+//    @PostConstruct
+//    public void doMyStartupStuff() {
+//        System.out.println("In doMyStartupStuff: " + getClass().getSimpleName());
+//    }
+//
+//    // define our destroy method
+//    @PreDestroy
+//    public void doMyCleanupStuff() {
+//        System.out.println("In doMyCleanupStuff: " + getClass().getSimpleName());
+//    }
 
     @Override
     public String getDailyWorkout() {
